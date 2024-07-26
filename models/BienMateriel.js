201c4340af4deaa5e1fd4f1baf19a720a235382c
Williest <williestnyainaandry@gmail.com>
2024-07-26 22:59:27 +0300

    fix: fix the getValeurAt method (tauxAmortissement) in BienMateriel

--- models/BienMateriel.js
+++ models/BienMateriel.js
@@ -9,21 +9,13 @@ class BienMateriel extends Possession{
     }
 
     getValeurAt(date){
-        // 1 an -> 10%       10% -> 100% pour 1 an cad 365 jrs                   365jrs  -> 10%
-        //                   x%  -> 10x% pour 0.x an cad x jrs(<365jrs)          xjrs    -> 10x/365
-        // il faut chercher l'algorithme quand date est pile egale a 1 an, date avant 1 an et date apres 1 an
-       
-        if(date.getTime() === this.dateAchat.getTime()+31556952000){
-            return this.valeur - (this.valeur * this.tauxAmortissement/100);
-        }
-        else if(date.getTime() < this.dateAchat.getTime()+31556952000  && date.getTime() >= this.dateAchat.getTime()){
-            return this.valeur - (this.valeur * 10)
-        }
+        // 1 an -> 10%      365jrs  -> 10%          
+        // x an -> 10x%     xjrs    -> (10x/365)%  
+        let dateDiff = (date.getTime() - this.dateAchat.getTime()) / (1000 * 3600 * 24);
+        return this.valeur - (this.valeur * (this.tauxAmortissement * (dateDiff/ 365) / 100));
     }
 }
 
-// let monPc = new BienMateriel("Williest", "3000","Mon bon vielle pc", "Informatique", 10, "2024-07-22");
-// console.log(monPc);
-// monPc.getValeurAt("la date choisie");
-
-console.log(new Date("2025-07-23").getDay() - new Date("2024-07-23").getDay());
+let monPc = new BienMateriel("Williest", "3000","Mon bon vieux pc", "Informatique", 10, new Date("2024-07-22"));
+console.log(monPc);
+console.log(monPc.getValeurAt(new Date('2024-07-22')));
