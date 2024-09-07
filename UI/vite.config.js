2eb1e7d058243339d16e425ae2cd4bbd8a5ffe45
Williest <williestnyainaandry@gmail.com>
2024-09-07 10:34:44 +0300

    feat: install serve in UI

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {port: process.env.PORT || 4000}
 })
+// Met
+// serve
+/**
+ * npm install serve
+ * script : {
+ *  "start" : "serve -s dist -l $PORT"
+ * }
+ */
